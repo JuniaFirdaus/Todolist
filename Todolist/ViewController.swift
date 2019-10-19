1819b10b51c68d7547c3984b88b8b56cafe25ddb
Junia <junia.firdaus@lintasmediadanawa.com>
2019-10-20 06:18:07 +0700

    Initial Commit

---
+++ Todolist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todolist
+//
+//  Created by PT Lintas Media Danawa on 20/10/19.
+//  Copyright © 2019 PT Lintas Media Danawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
